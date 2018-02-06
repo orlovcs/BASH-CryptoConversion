6b1f96af65be8bb89c8b158c9f28fd415a449bfd
Nikita <thetax45@gmail.com>
2018-02-06 14:25:42 -0500

    edit

--- coin.sh
+++ coin.sh
@@ -1,1 +1,1 @@
-zsh: no such file or directory: curl https://coinmarketcap.com/all/views/all/ | egrep /currencies/bitcoin/#markets
+curl https://coinmarketcap.com/all/views/all/ | egrep '(/currencies/bitcoin/#markets).*price'
